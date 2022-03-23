52a3af4cf0939b75ed88f66ddc1f51b6ac5f8239
Mahmoud Bdeir <mbdeir@aub.edu.lb>
2022-03-23 09:16:20 +0200

    Complete solution in Main (not yet in AllLogger)

---
+++ AllLogger/LogEntry.cs
@@ -0,0 +1,16 @@
+﻿namespace AllLogger
+{
+    public class LogEntry
+    {
+        public LogEntry(string fullClassName, string assemblyName, string[] parameters)
+        {
+            FullClassName = fullClassName;
+            AssemblyName = assemblyName;
+            Parameters = parameters;
+        }
+
+        public string FullClassName { get; }
+        public string AssemblyName { get; }
+        public object[] Parameters { get; }
+    }
+}
--- Logging/Program.cs
+++ Logging/Program.cs
@@ -1,5 +1,7 @@
-﻿using CMPS253.LoggerCore;
-using CMPS253.Loggers;
+﻿using AllLogger;
+using CMPS253.LoggerCore;
+using System.Reflection;
+using System.Runtime.Remoting;
 
 namespace CMPS253.Logging
 {
@@ -7,8 +9,41 @@ namespace CMPS253.Logging
     {
         static void Main(string[] args)
         {
-            ILogger logger = new AllLogger();
-            logger.Log("Test");
+            string? path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string json = File.ReadAllText("config.json");
+            var logs = Newtonsoft.Json.JsonConvert.DeserializeObject<List<LogEntry>>(json);
+            foreach (var item in logs)
+            {
+                ObjectHandle? T;
+                string assemblyFile = $"{path}\\{item.AssemblyName}";
+
+                if (item.Parameters == null)
+                {
+                    T = Activator.CreateInstanceFrom(assemblyFile, item.FullClassName);
+                }
+                else
+                {
+                    T = Activator.CreateInstanceFrom(
+                        assemblyFile: assemblyFile
+                        , typeName: item.FullClassName
+                        , ignoreCase: true
+                        , bindingAttr: BindingFlags.Instance | BindingFlags.Public
+                        , args: item.Parameters
+                        , binder: null
+                        , culture: null
+                        , activationAttributes: null
+                        );
+                }
+                (T.Unwrap() as ILogger).Log("hello");
+            }
+        }
+        public void WriteJsonFile()
+        {
+            List<LogEntry> logs = new();
+            logs.Add(new("CMPS253.Loggers.ConsoleLogger", "ConsoleLoggerLib.dll", null));
+            logs.Add(new("CMPS253.Loggers.FileLogger", "FileLoggerLib.dll", new string[] { @"c:\trash\log2.txt" }));
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(logs);
+            File.WriteAllText("config.json", json);
         }
     }
 }
